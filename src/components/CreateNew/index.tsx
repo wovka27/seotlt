e846db1555406796ba20f091796c93191af0c6aa
Vladimir Demyanchuk <wovka.pb@yandex.ru>
2025-03-29 01:57:08 +0500

    change CreateNew

--- src/components/CreateNew/index.tsx
+++ src/components/CreateNew/index.tsx
@@ -1,13 +1,9 @@
 import React, { FC } from 'react'
 import { observer } from 'mobx-react-lite'
 
-import UiButton from '@/components/UI/UiButton'
-
 import newsStore, { INewsItem } from '@/stores/news.store'
 import modalStore from '@/stores/modal.store'
 import ArticleForm from '@/components/ArticleForm'
-import UpdateNew from '@/components/UpdateNew'
-import Icon from '@/components/Icon'
 
 const CreateNew: FC = observer(() => {
   const apply = (data: INewsItem) => {
