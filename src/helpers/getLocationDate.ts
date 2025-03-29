220915b60fc94038a91aa99a75646160fc47340d
Vladimir Demyanchuk <wovka.pb@yandex.ru>
2025-03-29 23:30:06 +0500

    refactor

--- src/helpers/getLocationDate.ts
+++ src/helpers/getLocationDate.ts
@@ -1,5 +1,5 @@
 export const getLocationDate = (d: Date) =>
-  new Date(d).toLocaleDateString('ru-RU', {
+  new Date(d).toLocaleDateString('default', {
     day: 'numeric',
     month: 'numeric',
     year: 'numeric'
