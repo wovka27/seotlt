34fdf43a954f25791c3d368423957294df7f7bcc
Vladimir Demyanchuk <wovka.pb@yandex.ru>
2025-03-30 00:52:24 +0500

    refactor

--- src/helpers/getLocationDate.ts
+++
@@ -1,6 +0,0 @@
-export const getLocationDate = (d: Date, invert?: boolean) =>
-  new Date(d).toLocaleDateString(invert ? 'en-EN' : 'default', {
-    day: 'numeric',
-    month: 'numeric',
-    year: 'numeric'
-  })
