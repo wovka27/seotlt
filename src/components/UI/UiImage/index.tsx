000cb2bfcc9b78e9afeebb8cc215157c7edce5aa
Vladimir Demyanchuk <wovka.pb@yandex.ru>
2025-03-30 00:45:21 +0500

    refactor

--- src/components/UI/UiImage/index.tsx
+++ src/components/UI/UiImage/index.tsx
@@ -7,7 +7,6 @@ interface UiImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
 }
 
 const UiImage: React.FC<UiImageProps> = ({ src, alt = '', className = '', fallbackSize = 48, style, ...props }) => {
-  const [imgSrc] = useState(src)
   const [isLoading, setIsLoading] = useState(true)
   const [hasError, setHasError] = useState(false)
 
@@ -25,11 +24,10 @@ const UiImage: React.FC<UiImageProps> = ({ src, alt = '', className = '', fallba
     <div className={`ui-image ${className}`} style={style}>
       {!hasError && (
         <img
-          key={imgSrc}
-          src={imgSrc}
+          src={src}
           alt={alt}
           className={`ui-image__img ${isLoading ? 'ui-image__img--loading' : ''}`}
-          onChange={e => console.log(e)}
+          onChange={(e) => console.log(e)}
           onLoad={handleLoad}
           onError={handleError}
           {...props}
