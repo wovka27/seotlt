ea4bd7a66d09c55506c85fafb3b99cde5f14b5e7
Vladimir Demyanchuk <wovka.pb@yandex.ru>
2025-03-31 05:26:53 +0500

    refactor

--- src/components/CommentsList/CommentsListItem/index.tsx
+++ src/components/CommentsList/CommentsListItem/index.tsx
@@ -21,21 +21,23 @@ const CommentsListItem: React.FC<ICommentsListItemProps> = ({ data, onEdit, onRe
       <div className="comments-list-item-content">
         <div className="comments-list-item-content-header">
           <span className="comments-list-item-content-header__name">{data.user_name}</span>
-          <IconInfo
-            iconName="calendar"
-            size={14}
-            value={data.updated_at}
-            className="comments-list-item-content-header__date"
-          />
-          <Icon
-            name="pencil"
-            size={16}
-            className="comments-list-item-content-header__edit"
-            onClick={() => onEdit(data)}
-          />
-          <p className="comments-list-item-content-header__edit" onClick={() => onRemove(data.uuid)}>
-            Удалить
-          </p>
+          <div className="comments-list-item-content-header__actions">
+            <IconInfo
+              iconName="calendar"
+              size={14}
+              value={data.updated_at}
+              className="comments-list-item-content-header__date"
+            />
+            <Icon
+              name="pencil"
+              size={16}
+              className="comments-list-item-content-header__edit"
+              onClick={() => onEdit(data)}
+            />
+            <p className="comments-list-item-content-header__edit" onClick={() => onRemove(data.uuid)}>
+              Удалить
+            </p>
+          </div>
         </div>
         <p className="comments-list-item-content__text">{data.comment}</p>
       </div>
